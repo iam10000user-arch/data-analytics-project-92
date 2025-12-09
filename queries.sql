c8ae57d0bf52eb7075b73e2eb5208677733f2b18
iam10000user-arch <iam10000user@gmail.com>
2025-12-09 12:30:38 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
-"select count(customer_id) as customer_count from customers;"
-Берем Count чтобы сосчитать общее количество покупателей
+select count(customer_id) as customer_count from customers;
+We use Count to calculate the total number of customers
 
 SELECT 
     CONCAT(TRIM(e.first_name), ' ', TRIM(e.last_name)) AS seller,
@@ -31,9 +31,9 @@ JOIN products p ON s.product_id = p.product_id,
 GROUP BY e.first_name, e.last_name, avg_all.avg_income_all
 HAVING AVG(p.price * s.quantity) < avg_all.avg_income_all
 ORDER BY average_income ASC;
-Здесь мы используем подзапрос CTE для вычисления общей средней выручки по сделкам, далее берем все продажи и связываем их с продавцами и товарами. В ходе этого полключаем подзапрос, чтобы в каждой строчке иметь доступ к средней выручке по компании.
-Затем считаем среднюю выручку для каждого продавца. Group by - для группорировки продажи по каждому продавцу, а HAVING - сравнение средней выручки продавца с общей средней.
-  И order by - упорядочивает список от худшего к лучшему
+Here, we use a CTE subquery to calculate the total average revenue for transactions, then we take all the sales and associate them with sellers and products. We use a subquery to access the average revenue for each company in each row.
+Then, we calculate the average revenue for each seller. We use group by to group the sales by each seller, and we use having to compare the average revenue for each seller with the total average revenue.
+Finally, we use order by to sort the list from worst to best.
 
 SELECT 
     CONCAT(TRIM(e.first_name), ' ', TRIM(e.last_name)) AS seller,
@@ -46,8 +46,8 @@ JOIN products p ON s.product_id = p.product_id
 GROUP BY e.first_name, e.last_name, TO_CHAR(s.sale_date, 'Day'), EXTRACT(ISODOW FROM s.sale_date)
 ORDER BY day_num, seller;
 
-Здесь мы берем функцию TRIM чтобы собрать вместе фамилию и имя сотрудника, TO_CHAR - преобразует латц в дни недели, TRIM - убирвет ненужные пробелы, SUM - складывает все за день,FLOOR - округляет до целого в меньшую сторону.
-EXTRACT - дает нам номер дня недели, необходимо для сортировки, котрую сделаем с помощью ORDER BY в конце. Потом джойним таблицы и группируем по необходимым параметрам, проводим сортировку.
+Here we take the TRIM function to collect together the employee's last name and first name, TO_CHAR - converts the latz to days of the week, TRIM - removes unnecessary spaces, SUM - adds up everything for the day, FLOOR - rounds down to the nearest whole number.
+EXTRACT - gives us the day of the week number, which is necessary for sorting, which we will do using ORDER BY at the end. Then we join the tables and group them by the necessary parameters, and sort them.
 
 
 SELECT 
@@ -66,11 +66,10 @@ ORDER BY
         ELSE 3
     END;
 
-CASE END - распределяет покупателей по возрастным категориям.
-  Потом, смотрим кол-во покупателей в каждой категории
-Группируем по возрастным категориям
-И сортировку мы создаем вручную, по присваивая каждой категории свой номер
-
+CASE END - distributes customers by age category.
+ Then, we look at the number of customers in each category
+Grouped by age categories
+And we create the sorting manually by assigning a number to each category
 
 SELECT 
     TO_CHAR(s.sale_date, 'YYYY-MM') AS date,
@@ -81,12 +80,12 @@ JOIN products p ON s.product_id = p.product_id
 GROUP BY TO_CHAR(s.sale_date, 'YYYY-MM')
 ORDER BY date ASC;
 
-TO_СHAR - дата превращается в число
-COUNT DISTINCT - кол-во уникальных покупателей
-Потом считаем суммарную выручку за месяц
-Джойним таблицы
-Группируем данные по месяцам
-И сортируем от 1 до последнего месяца
+TO_CHAR - date is converted to a number
+COUNT DISTINCT - number of unique customers
+Then we calculate the total revenue for the month
+Join the tables
+Group the data by month
+And sort from 1 to the last month
 
 WITH first_sales AS (
     SELECT 
@@ -107,11 +106,13 @@ JOIN customers c ON s.customer_id = c.customer_id
 JOIN employees e ON s.sales_person_id = e.employee_id
 WHERE p.price = 0
 ORDER BY c.customer_id;
-Изначально используем подзапрос, находим самую ранную дату покупки
-Потом собираем имя и фамилию продавца и покупателя
-Джойним таблицы
-ставим условия, что первая покупка была акционной
-И сортируем покупателей по ID
+
+Initially, we use a subquery to find the earliest purchase date
+Then, we collect the names and surnames of the seller and the buyer
+We join the tables
+and set conditions that the first purchase was a promotional offer
+We sort the buyers by ID
+
 
 
 
