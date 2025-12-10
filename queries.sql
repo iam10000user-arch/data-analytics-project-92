27bb790602260b7896583f1ac3f94db0b7faf20c
iam10000user-arch <iam10000user@gmail.com>
2025-12-10 03:29:51 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -162,3 +162,4 @@ INNER JOIN customers AS c ON fs.customer_id = c.customer_id
 INNER JOIN employees AS e ON fs.sales_person_id = e.employee_id
 WHERE p.price = 0
 ORDER BY fs.customer_id;
+--Конец 
